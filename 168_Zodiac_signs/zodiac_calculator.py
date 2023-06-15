935aadde433ba7cc52bca9005b823c7aa66be789
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-06-14 20:07:25 -0400

    zodiac signs

---
+++ 168_Zodiac_signs/zodiac_calculator.py
@@ -0,0 +1,37 @@
+def calculate_zodiac_sign(day, month):
+    if (month == 1 and day >= 20) or (month == 2 and day <= 18):
+        return "Aquarius"
+    elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+        return "Pisces"
+    elif (month == 3 and day >= 21) or (month == 4 and day <= 19):
+        return "Aries"
+    elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
+        return "Taurus"
+    elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
+        return "Gemini"
+    elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
+        return "Cancer"
+    elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
+        return "Leo"
+    elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
+        return "Virgo"
+    elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
+        return "Libra"
+    elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
+        return "Scorpio"
+    elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
+        return "Sagittarius"
+    elif (month == 12 and day >= 22) or (month == 1 and day <= 19):
+        return "Capricorn"
+    else:
+        return "Invalid date"
+
+
+day = int(input("Enter the day of your birth: "))
+month = int(input("Enter the month of your birth (as a number): "))
+
+zodiac_sign = calculate_zodiac_sign(day, month)
+if zodiac_sign != "Invalid date":
+    print("Your zodiac sign is:", zodiac_sign)
+else:
+    print("Invalid date entered.")
