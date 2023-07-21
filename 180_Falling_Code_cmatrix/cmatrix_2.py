9ed93110267f0e83e56c7b84d3cd2de688b2b8d1
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-21 14:05:56 -0400

    more cmatrix with different display patterns - single character cycling, single column of characters, multi column of characters

---
+++ 180_Falling_Code_cmatrix/cmatrix_2.py
@@ -0,0 +1,43 @@
+import os
+import pygame as pg
+from random import choice, randrange
+
+
+class Symbol:
+    def __init__(self, x, y, speed):
+        self.x, self.y = x, y
+        self.speed = speed
+        self.value = choice(green_symbols)
+        self.interval = randrange(5, 30) # because we dont want static characters
+
+    def draw(self): # draw symbols at random intervals
+        frames = pg.time.get_ticks()
+        if not frames % self.interval:
+            self.value = choice(green_symbols)
+        self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE
+        surface.blit(self.value, (self.x, self.y))
+
+
+RES = WIDTH, HEIGHT = 1600, 900
+FONT_SIZE = 140
+
+
+pg.init() #initialize pygame module
+surface = pg.display.set_mode(RES)
+clock = pg.time.Clock() # to set the number of frames
+
+symbols = [chr(int('0x30a0', 16) + i) for i in range(96)]
+
+font = pg.font.Font('180_Falling_Code_cmatrix/MS Mincho.ttf', FONT_SIZE, bold=True) # font is MS Micho
+green_symbols = [font.render(char, True, pg.Color('green')) for char in symbols]
+
+symbol = Symbol(WIDTH//2 - FONT_SIZE//2, HEIGHT//2 - FONT_SIZE//2, speed=3)
+
+while True:
+    surface.fill(pg.Color('black'))
+
+    symbol.draw()
+
+    [exit() for i in pg.event.get() if i.type == pg.QUIT]
+    pg.display.flip()
+    clock.tick(60) # 60fps
--- 180_Falling_Code_cmatrix/cmatrix_4.py
+++ 180_Falling_Code_cmatrix/cmatrix_4.py
@@ -10,13 +10,12 @@ class Symbol:
         self.value = choice(green_symbols)
         self.interval = randrange(5, 30) # because we dont want static characters
 
-    def draw(self, color): # draw symbols at random intervals
+    def draw(self): # draw symbols at random intervals
         frames = pg.time.get_ticks()
         if not frames % self.interval:
-            self.value = choice(green_symbols if color == 'green' else lightgreen_symbols)
+            self.value = choice(green_symbols)
         self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE
         surface.blit(self.value, (self.x, self.y))
-
 
 class SymbolColumn:
     def __init__(self, x, y):
@@ -25,40 +24,27 @@ class SymbolColumn:
         self.symbols = [Symbol(x, i, self.speed) for i in range(y, y - FONT_SIZE * self.column_height, -FONT_SIZE)]
 
     def draw(self):
-        [symbol.draw('green') if i else symbol.draw('lightgreen') for i, symbol in enumerate(self.symbols)]
+        [symbol.draw() for symbol in self.symbols]
 
-
-# os.environ['SDL_VIDEO_CENTERED'] = '1'
 RES = WIDTH, HEIGHT = 1600, 900
-FONT_SIZE = 40
-alpha_value = 0
+FONT_SIZE = 50
+
 
 pg.init() #initialize pygame module
-screen = pg.display.set_mode(RES)
-surface = pg.Surface(RES)
-surface.set_alpha(alpha_value)
+surface = pg.display.set_mode(RES)
 clock = pg.time.Clock() # to set the number of frames
 
-# stuff to be displayed
-symbols = [str(i) for i in range(10)]
-letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-symbols = symbols + letters
+symbols = [chr(int('0x30a0', 16) + i) for i in range(96)]
 
 font = pg.font.Font('180_Falling_Code_cmatrix/MS Mincho.ttf', FONT_SIZE, bold=True) # font is MS Micho
-green_symbols = [font.render(char, True, (40, randrange(160, 256), 40)) for char in symbols]
-lightgreen_symbols = [font.render(char, True, pg.Color('lightgreen')) for char in symbols]
+green_symbols = [font.render(char, True, pg.Color('green')) for char in symbols]
 
 symbol_columns = [SymbolColumn(x, randrange(-HEIGHT, 0)) for x in range(0, WIDTH, FONT_SIZE)]
 
 while True:
-    screen.blit(surface, (0, 0))
     surface.fill(pg.Color('black'))
 
     [symbol_column.draw() for symbol_column in symbol_columns]
-
-    if not pg.time.get_ticks() % 20 and alpha_value < 170:
-        alpha_value += 6
-        surface.set_alpha(alpha_value)
 
     [exit() for i in pg.event.get() if i.type == pg.QUIT]
     pg.display.flip()
