d6af574484c722d8d8e2abc160c7a9a74262bd07
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-31 16:27:26 -0400

    all emoji sequences printed

--- 214_Emojis/all_emoji_sequences.py
+++ 214_Emojis/all_emoji_sequences.py
@@ -14,7 +14,7 @@ def convert_to_emoji(emoji_code_points):
     return emoji_str
 
 if __name__ == "__main__":
-    emoji_data_file = "./emoji-test.txt"
+    emoji_data_file = "./emoji-sequences.txt"
     emoji_code_points = extract_emojis(emoji_data_file)
 
     # print(emoji_code_points)
@@ -26,7 +26,19 @@ if __name__ == "__main__":
     # print(split_list)
 
     # for sublist in split_list:
-    #     print(sublist)
+        # print(sublist)
+        # break
 
-    for sublist in split_list:
-        print(sublist,convert_to_emoji(sublist))
+    for i,sublist in enumerate( split_list):
+        # print(sublist)
+        if ".." in sublist[0]:
+            start_str, end_str = sublist[0].split("..")
+            start_code_point = int(start_str, 16)
+            end_code_point = int(end_str, 16)
+            code_points = [code_point for code_point in range(start_code_point, end_code_point + 1)]
+            print(["{:X}".format(code) for code in code_points],convert_to_emoji(["{:X}".format(code) for code in code_points]))
+        else:
+            print(sublist,convert_to_emoji(sublist))
+        # if i == 10:
+        #     break
+        
