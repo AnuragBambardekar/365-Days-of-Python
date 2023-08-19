e58be7cfb8b1e94376bf44d439fd24e959d7c6f3
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-08-19 12:03:19 -0400

    use any word instead of print

---
+++ 1_Print_commands_in_Python/log_=_print.py
@@ -0,0 +1,8 @@
+log = print
+log("Hello World!")
+
+# print is an in-built function so you can assign it to any variable and use that variable to print instead
+
+drake = print
+drake("GOAT")
+drake("6GOD")
