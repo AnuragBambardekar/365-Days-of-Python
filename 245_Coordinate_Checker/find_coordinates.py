224dda9feb3651169ee2a6ebd9ad994bbc90cb76
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-08-31 09:00:06 -0400

    finding the screen coordinates

---
+++ 245_Coordinate_Checker/find_coordinates.py
@@ -0,0 +1,12 @@
+# Find the coordinates on your screen
+
+import pyautogui, sys
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\n')
