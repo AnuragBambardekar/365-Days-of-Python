adc25f0928eb52dc4c95c204cf1b400b2a166c5e
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-09-04 09:50:41 -0400

    most frequently occuring element in a list

---
+++ 118_Zen_Of_python_(Easter_eggs)/mostFrequent.py
@@ -0,0 +1,20 @@
+"""
+# Long Method
+x = [1,2,1,3,4,1,2,4,1]
+
+freq_dict = {}
+
+for item in x:
+    if item in freq_dict:
+        freq_dict[item] += 1
+    else:
+        freq_dict[item] = 1
+
+most_frequent = max(freq_dict, key=freq_dict.get)
+print(most_frequent)
+"""
+
+x = [1,2,1,3,4,1,2,4,1]
+
+most = max(x, key=x.count)
+print(most)
