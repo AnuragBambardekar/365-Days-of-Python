d3704fa3b4a0bb76f7022ef824ec7f84e1ee981b
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-10-25 11:29:37 -0400

    singly linked list implementation

---
+++ 298_LinkedLists/singly_LL.py
@@ -0,0 +1,64 @@
+# Singly Linked list
+
+class ListNode:
+    def __init__(self, val, next_node = None) -> None:
+        self.val = val
+        self.next = next_node
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = ListNode(-1)
+        self.tail = self.head
+
+    def get(self, index):
+        curr = self.head.next
+        i=0
+        while curr:
+            if i==index:
+                return curr.val
+            i+=1
+            curr=curr.next
+        return -1
+    
+    def insertHead(self, val):
+        new_node = ListNode(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+        if not new_node.next:
+            self.tail = new_node
+
+    def insertTail(self, val):
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next
+
+    def remove(self, index):
+        i=0
+        curr = self.head
+        while i < index and curr:
+            i+=1
+            curr = curr.next
+        
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
+    
+    def getValues(self):
+        curr = self.head.next
+        res = []
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+        return res
+    
+linkedList = LinkedList()
+linkedList.insertHead(1)
+linkedList.insertTail(2)
+linkedList.insertTail(3)
+
+print(linkedList.getValues())
+print(linkedList.get(1))
+print(linkedList.remove(1))
+print(linkedList.getValues())
