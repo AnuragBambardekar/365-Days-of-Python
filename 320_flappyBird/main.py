20bca3ae5ab0b6007fee9dac3147b0f40dde8f6a
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-11-16 15:13:47 -0500

    game over logic - reset game

--- 320_flappyBird/main.py
+++ 320_flappyBird/main.py
@@ -31,12 +31,20 @@ pass_pipe = False
 # load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+button_img = pygame.image.load('img/restart.png')
 
 # Add text
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x,y))
 
+# Reset Game
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height/2)
+    score = 0
+    return score
 
 # Add bird
 class Bird(pygame.sprite.Sprite):
@@ -117,6 +125,28 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0: # < 200 to check pipe being killed
             self.kill()
 
+# Button
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+    def draw(self):
+        action = False
+        
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        # Draw button
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
 
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
@@ -128,6 +158,9 @@ bird_group.add(flappy)
 # top_pipe = Pipe(300, int(screen_height/2), 1)
 # pipe_group.add(bottom_pipe)
 # pipe_group.add(top_pipe)
+
+# create restart button instance
+button = Button(screen_width//2-50, screen_height//2-100, button_img)
 
 # Run Game - Main game loop
 run = True
@@ -189,6 +222,14 @@ while run:
         if abs(ground_scroll) > 35:
             ground_scroll = 0
         pipe_group.update()
+
+    # check for game over and reset
+    if game_over == True:
+        if button.draw() == True:
+            # print("Clicked")
+            game_over = False
+            score = reset_game()
+
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
